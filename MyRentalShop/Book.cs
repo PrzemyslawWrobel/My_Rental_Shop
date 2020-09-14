3b74fec857034ebca5da9c08ebb0a7547ca4a712
PW <przemek.wrobel@poczta.fm>
2020-09-14 14:45:19 +0200

    Dodany opis właściwości

--- MyRentalShop/Book.cs
+++ MyRentalShop/Book.cs
@@ -6,11 +6,34 @@ namespace MyRentalShop
 {
     public class Book
     {
+        /// <summary>
+        /// Nr identyfikacyjny książki
+        /// </summary>
         public int IdBook { get; set; }
+
+        /// <summary>
+        /// Tytuł skiążki
+        /// </summary>
         public string Title { get; set; }
+
+        /// <summary>
+        /// Pełna nazwa autora książki
+        /// </summary>
         public string Author { get; set; }
+
+        /// <summary>
+        /// Opis książki.
+        /// </summary>
         public string Description { get; set; }
+
+        /// <summary>
+        /// Rok wydania książki
+        /// </summary>
         public int Edition { get; set; }
+
+        /// <summary>
+        /// Gatunek książki(np dla dzieci, sensacyjna)
+        /// </summary>
         public int TypeId { get; set; }
     }
 }
